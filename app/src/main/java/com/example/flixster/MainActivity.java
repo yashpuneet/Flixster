a4bb8c352a344dd492552ac18483c38acdef46b2
yashpuneet <yash.puneet02@gmail.com>
2021-09-22 17:19:14 -0700

    Connected to database and returned JSON object

---
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.flixster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import okhttp3.Headers;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client .get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json)
+            {
+                Log.d(TAG, "onSuccess");
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable)
+            {
+                Log.d(TAG, "onFailure");
+            }
+        });
+    }
+}
